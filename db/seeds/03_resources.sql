9044d62658dd54d93aad477f08f7c601deab9ded
ibifuro odu <oduibifuro@gmail.com>
2022-02-14 18:42:25 -0500

    Created BE route for displaying resources

---
+++ db/seeds/03_resources.sql
@@ -0,0 +1,37 @@
+INSERT INTO resources (user_id,title,description, category, resource_link) 
+VALUES (1,
+'nowhere',
+'lorem ipsum isnit',
+'boxing',
+'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350');
+
+INSERT INTO resources (user_id,title,description, category, resource_link) 
+VALUES (2,
+'waterfront',
+'It has survived not only five centuries, but also the leap into electronic typesetting, 
+remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages',
+'boxing',
+'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350');
+
+INSERT INTO resources (user_id,title,description, category, resource_link) 
+VALUES (3,
+'Creator',
+'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters',
+'boxing',
+'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350');
+
+INSERT INTO resources (user_id,title,description, category, resource_link) 
+VALUES (4,
+'Whoville',
+'lorem ipsum isnit',
+'boxing',
+'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350');
+
+INSERT INTO resources (user_id,title,description, category, resource_link) 
+VALUES (5,
+'learning',
+'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable',
+'boxing',
+'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350');
+
