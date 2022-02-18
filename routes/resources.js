50940da7889dac8a1de55b3312594dadeb6959c3
ibifuro odu <oduibifuro@gmail.com>
2022-02-17 23:57:38 -0500

    more likes and ratings

--- routes/resources.js
+++ routes/resources.js
@@ -1,7 +1,23 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+  /*All post routes*/
+
+  router.get("/", (req, res) => {
+    db.query(
+      `SELECT resources.*, users.name FROM resources JOIN Users ON resources.user_id = users.id;`
+    )
+      .then((data) => {
+        const resources = data.rows;
+        res.render("resources", { resources });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   router.post("/new", (req, res) => {
     const userID = req.session.user_id;
     const queryString = `INSERT INTO resources(
@@ -30,9 +46,60 @@ module.exports = (db) => {
       });
   });
 
+  router.post("/searchResults", (req, res) => {
+    const queryParams = [];
+    let queryString = `SELECT resources.*, users.name FROM resources JOIN Users ON users.id =
+      resources.user_id WHERE resources.id IS NOT NULL `;
+  
+    //validate that search queries exist and then add on to the query
+    if (!req.body.title == "") {
+      queryParams.push(`%${req.body.title}%`);
+      queryString += ` AND resources.title LIKE $${queryParams.length} `;
+    }
+  
+    if (!req.body.category == "") {
+      queryParams.push(req.body.category);
+      queryString += `AND resources.category = $${queryParams.length} `;
+    }
+    queryString += `GROUP BY resources.id, users.name ;`;
+  
+    db.query(queryString, queryParams)
+      .then((data) => {
+        if (data) {
+          const resources = data.rows;
+          res.render("resources", { resources });
+          return;
+        }
+        res.send("No matching search results");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.post("/comments", (req, res) => {
+    const userID = req.session.user_id;
+    const queryString = `INSERT INTO comments(
+        user_id,
+        resource_id,
+        comment) VALUES ($1,$2,$3) RETURNING *;`;
+  
+    const values = [userID, req.body.postID, req.body.comment];
+  
+    db.query(queryString, values)
+      .then((data) => {
+        const newResource = data.rows;
+        // res.send({ newResource });
+        res.redirect("/resources");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   router.post("/likes", (req, res) => {
     const userID = req.session.user_id;
-    //console.log(req.body.id);
+
     const queryString = `INSERT INTO likes(
     user_id,
     resource_id,
@@ -42,19 +109,27 @@ module.exports = (db) => {
 
     db.query(queryString, values)
       .then((data) => {
-        data.rows;
+        res.json(data.rows);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/likes", (req, res) => {
-    db.query(
-      `SELECT likes.*, users.name FROM likes JOIN users ON users.id = likes.user_id`
-    )
+  router.post("/likes/delete", (req, res) => {
+
+    const userID = req.session.user_id;
+    const queryString = `DELETE FROM likes 
+    WHERE user_id = $1 AND resource_id = $2;`;
+
+    const values = [userID, req.body.postID];
+    db.query(queryString, values)
       .then((data) => {
-        res.json(data.rows);
+        if (data.rows.length > 0) {
+          res.status(200).json('success');
+        } else {
+          res.status(202).json('failed');
+        }
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
@@ -73,18 +148,6 @@ module.exports = (db) => {
 
     db.query(queryString, values)
       .then((data) => {
-        data.rows;
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  router.get("/ratings", (req, res) => {
-    db.query(
-      `SELECT ratings.*, users.name FROM ratings JOIN users ON users.id = ratings.user_id`
-    )
-      .then((data) => {
         res.json(data.rows);
       })
       .catch((err) => {
@@ -92,24 +155,38 @@ module.exports = (db) => {
       });
   });
 
-  router.post("/comments", (req, res) => {
+  router.post("/ratings/delete", (req, res) => {
+
     const userID = req.session.user_id;
-    const queryString = `INSERT INTO comments(
-      user_id,
-      resource_id,
-      comment) VALUES ($1,$2,$3) RETURNING *;`;
-
-    const values = [userID, req.body.postID, req.body.comment];
-
+    const queryString = `DELETE FROM ratings 
+      WHERE user_id = $1 AND resource_id = $2;`;
+  
+    const values = [userID, req.body.postID];
     db.query(queryString, values)
       .then((data) => {
-        const newResource = data.rows;
-        // res.send({ newResource });
-        res.redirect("/resources");
+        if (data.rows.length > 0) {
+          res.status(200).json('success');
+        } else {
+          res.status(202).json('failed');
+        }
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
+  });
+
+  /*Post routes end*/
+
+  /*Get routes start*/
+  
+  router.get("/new", (req, res) => {
+    // makes a request to localhost:3000/resources/new
+    const id = req.session.user_id;
+    if (id) {
+      res.render("create_resource");
+    } else {
+      res.redirect("/users/login");
+    }
   });
 
   router.get("/comments", (req, res) => {
@@ -123,7 +200,7 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
-
+  
   router.get("/comments/:postID", (req, res) => {
     const value = req.params.postID;
     const queryString = `SELECT comments.*, users.name FROM comments JOIN Users ON users.id = comments.user_id WHERE comments.resource_id = $1 ;`;
@@ -136,59 +213,72 @@ module.exports = (db) => {
       });
   });
 
-  router.post("/searchResults", (req, res) => {
-    const queryParams = [];
-    let queryString = `SELECT resources.*, users.name FROM resources JOIN Users ON users.id =
-    resources.user_id WHERE resources.id IS NOT NULL `;
+  router.get("/likes/:postID", (req, res) => {
 
-    //validate that search queries exist and then add on to the query
-    if (!req.body.title == "") {
-      queryParams.push(`%${req.body.title}%`);
-      queryString += ` AND resources.title LIKE $${queryParams.length} `;
-    }
+    const userID = req.session.user_id;
+    const postID = req.params.postID;
+    const queryString = `SELECT likes.*, users.name 
+  FROM likes 
+  JOIN users ON users.id = likes.user_id 
+  WHERE likes.resource_id = $1 AND users.id = $2;`;
+    const values = [postID, userID];
 
-    if (!req.body.category == "") {
-      queryParams.push(req.body.category);
-      queryString += `AND resources.category = $${queryParams.length} `;
-    }
-    queryString += `GROUP BY resources.id, users.name ;`;
-
-    db.query(queryString, queryParams)
+    db.query(queryString, values)
       .then((data) => {
-        if (data) {
-          const resources = data.rows;
-          res.render("resources", { resources });
-          return;
+        let responseobject = {};
+
+        if (data.rows.length > 0) {
+          responseobject["status"] = "found";
+          res.status(200).json(responseobject);
+        } else {
+          responseobject["status"] = "not found";
+          res.status(202).json(responseobject);
         }
-        res.send("No matching search results");
+
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
+ 
 
-  router.get("/", (req, res) => {
+  router.get("/ratings", (req, res) => {
     db.query(
-      `SELECT resources.*, users.name FROM resources JOIN Users ON users.id = resources.user_id;`
+      `SELECT ratings.*, users.name FROM ratings JOIN users ON users.id = ratings.user_id`
     )
       .then((data) => {
-        const resources = data.rows;
-        res.render("resources", { resources });
+        res.json(data.rows);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/new", (req, res) => {
-    // makes a request to localhost:3000/resources/new
-    const id = req.session.user_id;
-    if (id) {
-      res.render("create_resource");
-    } else {
-      res.redirect("/users/login");
-    }
-  });
+  router.get("/ratings/:postID", (req, res) => {
 
-  return router;
+    const userID = req.session.user_id;
+    const values = [req.params.postID, userID];
+
+    const queryString = `SELECT ratings.*, users.name 
+    FROM ratings 
+    JOIN users ON users.id = ratings.user_id 
+    WHERE ratings.resource_id = $1 AND users.id = $2;`;
+
+    db.query(queryString, values)
+      .then((data) => {
+
+        if (data.rows.length > 0) {
+          res.status(200).json(data.rows[0].rating);
+        } else {
+          res.status(202).json(0);
+        }
+      })
+      .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+return router;
 };
+
+/*Get routes start*/
